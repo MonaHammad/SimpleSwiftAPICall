a6dd25e9272049e98907ade40e84fc25055e413f
Mona Hammad <monahammad@Monas-MacBook-Pro.local>
2019-12-01 23:01:26 +0200

    get and post functions

--- SimpleAPICall/SimpleAPICall/ViewController.swift
+++ SimpleAPICall/SimpleAPICall/ViewController.swift
@@ -14,7 +14,42 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func getCalled(_ sender: Any) {
+        let url = URL(string: "https://httpbin.org/get")!
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print("error: \(error)")
+            } else {
+                if let response = response as? HTTPURLResponse {
+                    print("statusCode: \(response.statusCode)")
+                }
+                if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                    print("data: \(dataString)")
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    @IBAction func postCalled(_ sender: Any) {
+        let url = URL(string: "https://httpbin.org/post")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                print("error: \(error)")
+            } else {
+                if let response = response as? HTTPURLResponse {
+                    print("statusCode: \(response.statusCode)")
+                }
+                if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                    print("data: \(dataString)")
+                }
+            }
+        }
+        task.resume()
+    }
+    
 }
 
