baf71830a96297ccffe4106af000d9b0d7b6fa03
Mona Hammad <monahammad@Monas-MacBook-Pro.local>
2019-12-01 22:52:40 +0200

    initial commit

---
+++ SimpleAPICall/SimpleAPICall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleAPICall
+//
+//  Created by Mona Hammad on 12/1/19.
+//  Copyright © 2019 Mona Hammad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
